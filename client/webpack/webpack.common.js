7b1d0c422bd3e418d29578ae8519915085757f3b
unknown <you@example.com>
2020-09-15 23:59:01 +0700

    update files

--- client/webpack/webpack.common.js
+++ client/webpack/webpack.common.js
@@ -117,6 +117,8 @@ module.exports = {
   ],
   resolve: {
     modules: [resolve(process.cwd(), 'src'), 'node_modules'],
-    extensions: ['*', '.js', '.jsx', '.json']
+    extensions: ['*', '.js', '.jsx', '.json'],
+    symlinks: false,
+    cacheWithContext: false
   }
 }
