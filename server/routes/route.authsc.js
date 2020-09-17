a1bb053506b38ebed2f55c965753968836be84b4
unknown <you@example.com>
2020-09-17 17:45:43 +0700

    push files

--- server/routes/route.authsc.js
+++ server/routes/route.authsc.js
@@ -1,29 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const CLIENT_URL = process.env.NODE_ENV !== 'production' ? process.env.CLIENT_URL_DEV : process.env.CLIENT_URL_PROD
 
 /**
  * @description third party authentication
  */
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
 router.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-  return process.env !== 'prodcution'
-    ? res.status(200).redirect(`${process.env.CLIENT_URL_DEV}`)
-    : res.status(200).redirect(`${process.env.CLIENT_URL_PROD}`)
+  return res.status(200).redirect(`${CLIENT_URL}`)
 })
 
 router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }))
 router.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => {
-  return process.env !== 'prodcution'
-    ? res.status(200).redirect(`${process.env.CLIENT_URL_DEV}`)
-    : res.status(200).redirect(`${process.env.CLIENT_URL_PROD}`)
+  return res.status(200).redirect(`${CLIENT_URL}`)
 })
 
 router.get('/auth/github', passport.authenticate('github', { scope: ['user:email', 'read:user'] }))
 router.get('/auth/github/callback', passport.authenticate('github'), (req, res) => {
-  return process.env !== 'prodcution'
-    ? res.status(200).redirect(`${process.env.CLIENT_URL_DEV}`)
-    : res.status(200).redirect(`${process.env.CLIENT_URL_PROD}`)
+  return res.status(200).redirect(`${CLIENT_URL}`)
 })
 
 module.exports = router
